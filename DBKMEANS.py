1891c551135f5a615069a9e5ac93d34943a0897d
RevolvingOcelot <122363827+RevolvingOcelot@users.noreply.github.com>
2025-07-03 14:20:31 +0200

    Add files via upload
    run imrec for dbkmeans tests

---
+++ DBKMEANS.py
@@ -0,0 +1,295 @@
+import math
+import numpy as np
+from kmeans import KMeans as kme
+
+
+class DBKMEANS:
+    def fit(self, data_points, eps, minpts, kclust):
+        k = len(data_points[0])
+        root = self.setup_tree(data_points, k)
+        labels = [-1] * len(data_points)
+        clusters = []
+        noise = []
+        visited = set()
+
+        for index, point in enumerate(data_points):
+            if index in visited:
+                continue
+            visited.add(index)
+
+            neighbours = self.nearest_neighbours(root, point, eps, k)
+
+            if len(neighbours) + 1 < minpts:
+                noise.append(point)
+            else:
+                cluster = []
+                self.expand_neighbours(index, data_points, point, eps, minpts, cluster, clusters, neighbours, labels,
+                                       visited, root, k)
+                clusters.append(cluster)
+
+        clusters = [np.array(cluster) for cluster in clusters]
+
+        cluster_centers = []
+        cluster_sizes = []
+        m = len(clusters)
+        k = kclust
+
+        for cluster in clusters:
+            center = cluster.mean(axis=0)
+            size = len(cluster)
+            cluster_centers.append(center)
+            cluster_sizes.append(size)
+        print(len(cluster_centers))
+        if m > k:
+            clusters, cluster_centers = merge_clusters(clusters, cluster_centers, k)
+        elif m < k:
+            clusters, cluster_centers = split_clusters(clusters, cluster_centers, k)
+
+        print(len(cluster_centers))
+        return np.array(cluster_centers)
+
+
+    def expand_neighbours(self, index, data_points, point, eps, minpts, cluster, clusters, neighbours, labels, visited,
+                          root, k):
+        cluster.append(data_points[index])
+        labels[index] = len(clusters)
+        queue = neighbours.copy()
+        while queue:
+            neighbour_index = queue.pop(0)
+            if neighbour_index not in visited:
+                visited.add(neighbour_index)
+                new_neighbours = self.nearest_neighbours(root, data_points[neighbour_index], eps, k)
+                if len(new_neighbours) + 1 >= minpts:
+                    temp_list = []
+                    for x in new_neighbours:
+                        if x not in queue:
+                            temp_list.append(x)
+                    queue.extend(temp_list)
+
+            if labels[neighbour_index] == -1:
+                labels[neighbour_index] = len(clusters)
+                cluster.append(data_points[neighbour_index])
+
+    def setup_tree(self, data_points, k):
+        root = None
+        for index, x in enumerate(data_points):
+            root = insert(root, x, index, k)
+        return root
+
+    def nearest_neighbours(self, root, point, eps, k, depth=0, neighbours=None):
+        if root is None:
+            return neighbours
+
+        if neighbours is None:
+            neighbours = []
+
+        d = self.distance(point, root.point)
+        if d <= eps:
+            neighbours.append(root.index)
+        cd = depth % k  # cd - current dimension, each layer of the tree the dimension changes to narrow down on the point quicker
+
+        if point[cd] < root.point[
+            cd]:  # if x/y of the point is less/more than the x/y of the current node then go left/right down the tree
+            self.nearest_neighbours(root.left, point, eps, k, depth + 1, neighbours)
+            if abs(point[cd] - root.point[
+                cd]) <= eps:  # if the current node is in eps distance of the point, there may be a closer point on the other side of the tree, obscured by the dimension cutoff
+                self.nearest_neighbours(root.right, point, eps, k, depth + 1, neighbours)
+        else:
+            self.nearest_neighbours(root.right, point, eps, k, depth + 1, neighbours)
+            if abs(point[cd] - root.point[cd]) <= eps:
+                self.nearest_neighbours(root.left, point, eps, k, depth + 1, neighbours)
+        return neighbours
+
+    def distance(self, point1=[], point2=[]):
+        total = 0
+        for n in range(0, len(point1)):
+            total += (point1[n] - point2[n]) ** 2
+        return math.sqrt(total)
+
+
+class Node:
+    def __init__(self, point, index):
+        self.point = point
+        self.left = None
+        self.right = None
+        self.index = index
+
+
+def newNode(point, index):
+    return Node(point, index)
+
+
+def insert(root, point, index, k, depth=0):
+    if not root:
+        return newNode(point, index)
+
+    cd = depth % k
+
+    if point[cd] < root.point[cd]:
+        root.left = insert(root.left, point, index, k, depth + 1)
+    else:
+        root.right = insert(root.right, point, index, k, depth + 1)
+    return root
+
+def compute_cluster_center(cluster):
+    return np.mean(cluster, axis=0)
+
+def compute_cluster_density(cluster):
+    center = compute_cluster_center(cluster)
+    distances = np.linalg.norm(cluster - center, axis=1)
+    # Lower average distance means higher density
+    return len(cluster) / (np.mean(distances) + 1e-10)
+
+def merge_clusters(clusters,cluster_centers, k):
+    while len(clusters) > k:
+        # Compute density for each cluster
+        densities = [compute_cluster_density(c) for c in clusters]
+        sizes = [len(c) for c in clusters]
+
+        # Choose two clusters to merge: e.g., smallest size + lowest density
+        c1 = np.argmin(densities)
+        c2 = np.argmin(sizes)
+
+        # Merge and update clusters
+        merged = np.vstack((clusters[c1], clusters[c2]))
+        merged_points = np.vstack((clusters[c1], clusters[c2]))
+        new_center = merged_points.mean(axis=0)
+        clusters.pop(max(c2,c1))
+        clusters.pop(min(c2,c1))
+        cluster_centers.pop(c1)
+        cluster_centers.pop(c2)
+        clusters.append(merged)
+        cluster_centers.append(new_center)
+    return clusters,cluster_centers
+
+def split_clusters(clusters, cluster_centers, k=2):
+    densities = [compute_cluster_density(c) for c in clusters]
+    idx = np.argmin(densities)
+    cluster_to_split = clusters.pop(idx)
+    cluster_centers.pop(idx)
+
+    if len(cluster_to_split) < k:
+        print("Not enough points to split this cluster")
+        return clusters, cluster_centers
+
+    # Run your KMeans on this cluster
+    kmeans = kme(n_clusters=k)
+    labels = kmeans.fit_predict(cluster_to_split)
+
+    # Form new clusters
+    new_clusters = []
+    new_centers = []
+    for i in range(k):
+        points = cluster_to_split[labels == i]
+        if len(points) == 0:
+            continue  # avoid empty cluster
+        new_clusters.append(points)
+        new_centers.append(points.mean(axis=0))
+
+    # Add new splits
+    print("hi")
+    print(len(cluster_centers))
+    print(len(clusters))
+    clusters.extend(new_clusters)
+    cluster_centers.extend(new_centers)
+    print(len(cluster_centers))
+
+    return new_clusters, new_centers
+
+class KMeans:
+    def __init__(self, n_clusters=3, max_iter=500, tol=1e-10, init='kmeans++', alg='lloyd',eps=8.5, minpts=120):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.tol = tol
+        self.init = init  # 'random' or 'kmeans++'
+        self.alg = alg
+        self.eps = eps
+        self.minpts = minpts
+
+    def fit(self, X_train):
+        uncap = False
+        n_samples, n_features = X_train.shape
+        if self.alg == 'lloyd':
+            self.centroids = DBKMEANS.fit(data_points=X_train, eps=self.eps, minpts=self.minpts,kclust=self.n_clusters)
+
+            for iteration in range(self.max_iter):
+                prev_centroids = self.centroids.copy()
+
+                # Vectorized distance computation
+                distances = np.linalg.norm(X_train[:, np.newaxis, :] - self.centroids[np.newaxis, :, :], axis=2)
+                labels = np.argmin(distances, axis=1)
+
+                for i in range(self.n_clusters):
+                    points_in_cluster = X_train[labels == i]
+                    if len(points_in_cluster) > 0:
+                        self.centroids[i] = points_in_cluster.mean(axis=0)
+
+                shifts = np.linalg.norm(self.centroids - prev_centroids, axis=1)
+                if np.all(shifts <= self.tol) and  uncap == False:
+                    print("it = ",iteration)
+                    break
+            print("maxx")
+        else:
+            ddd = DBKMEANS()
+            self.centroids = ddd.fit(data_points=X_train,eps=8.4,minpts=120,kclust=self.n_clusters)
+            labels = np.zeros(n_samples, dtype=int)
+            upper_bounds = np.full(n_samples, np.inf)
+            lower_bounds = np.zeros((n_samples, self.n_clusters))
+
+            for it in range(self.max_iter):
+                prev_centroids = self.centroids.copy()
+
+
+                centroid_distances = distance_matrix(self.centroids, self.centroids)
+                np.fill_diagonal(centroid_distances, np.inf)
+                s = 0.5 * np.min(centroid_distances, axis=1)
+
+
+
+                needs_update = (upper_bounds > s[labels])
+                if np.any(needs_update):
+                    distances = distance_matrix(X_train[needs_update], self.centroids)
+                    new_labels = np.argmin(distances, axis=1)
+                    upper_bounds[needs_update] = distances[np.arange(len(new_labels)), new_labels]
+                    lower_bounds[needs_update] = distances
+
+                    labels[needs_update] = new_labels
+
+                for k in range(self.n_clusters):
+                    points = X_train[labels == k]
+                    if len(points) > 0:
+                        self.centroids[k] = np.mean(points, axis=0)
+
+                # Step 3: Update bounds
+                centroid_shifts = np.linalg.norm(self.centroids - prev_centroids, axis=1)
+                upper_bounds += centroid_shifts[labels]
+                lower_bounds -= centroid_shifts[np.newaxis, :]
+                lower_bounds = np.maximum(lower_bounds, 0)
+
+                if np.max(centroid_shifts) <= self.tol and uncap == False:
+                    print(f"Converged in {it + 1} iterations")
+                    break
+
+
+
+    def evaluate(self, X):
+        centroids = []
+        centroid_ids = []
+        for x in X:
+            dist = distance(x, self.centroids)
+            centroid_id = np.argmin(dist)
+            centroids.append(self.centroids[centroid_id])
+            centroid_ids.append(centroid_id)
+        return centroids, centroid_ids
+
+    def fit_predict(self, X):
+        self.fit(X)
+        _, cluster_indices = self.evaluate(X)
+        return np.array(cluster_indices)
+
+def distance(point, data):
+    return np.sqrt(np.sum((point - data)**2, axis=1))
+
+def distance_matrix(X, Y):
+    # Fast Euclidean distance matrix: ||X - Y||^2
+    return np.linalg.norm(X[:, np.newaxis, :] - Y[np.newaxis, :, :], axis=2)
--- imrec.py
+++ imrec.py
@@ -1,4 +1,3 @@
-
 from sklearn.manifold import TSNE
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -9,11 +8,29 @@ from sklearn.cluster import DBSCAN
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 from skimage import metrics
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
+from sklearn.metrics import (
+    adjusted_rand_score,
+    normalized_mutual_info_score,
+    adjusted_mutual_info_score,
+    homogeneity_score,
+    completeness_score,
+    v_measure_score
+)
+from sklearn.cluster import KMeans
+import umap
+import numpy as np
+from sklearn.metrics import silhouette_score
+
+from kmeans import KMeans as kme
+
+import os
+import time
+from DBKMEANS import KMeans as dbk
 
 
-
-data = pd.read_pickle('./data.pkl')
-x_train, x_test, y_train, y_test = train_test_split(data["file"], data["label"], test_size=0.2, shuffle=True, random_state=11)
+data = pd.read_pickle('./datae1.pkl')
+x_train, x_test, y_train, y_test = train_test_split(data["embed"], data["label"], test_size=0.2, shuffle=True, random_state=11)
 
 x_train = np.array(x_train)
 x_test = np.array(x_test)
@@ -26,28 +43,177 @@ y_train = y_train[:8000] #these splits arent actually necessary with DBSCAN but 
 x_test = x_test[:2000]
 y_test = y_test[:2000]
 
-nsamples, nx, ny, nrgb = x_train.shape
-x_trained = x_train.reshape((nsamples, nx * ny * nrgb))
-nsamples, nx, ny, nrgb = x_test.shape
-x_tested = x_test.reshape((nsamples, nx * ny * nrgb))
+
+# File to store preprocessed data
+reduced_file = "pca_tsne_outputs.npz"
+
+to_save = False
+
+if os.path.exists(reduced_file) and to_save is True:
+    print("Loading cached PCA and t-SNE outputs")
+    data_reduced = np.load(reduced_file)
+    x_trained_ts = data_reduced["x_tsne"]
+    # Optional: x_pca = data_reduced["x_pca"] if you need it later
+else:
+
+    # Step 1: PCA
+    pca = PCA(n_components=50)
+    x_pca = pca.fit_transform(x_train)
+
+    # Step 2: t-SNE
+    tsne = TSNE(n_components=2, random_state=42)
+    x_trained_ts = tsne.fit_transform(x_pca)
+
+    # Save results
+    np.savez(reduced_file, x_pca=x_pca, x_tsne=x_trained_ts)
+    print(f"Saved PCA and t-SNE outputs to {reduced_file}")
+
+#umap_model = umap.UMAP(n_components=2, random_state=42)
+#x_trained_ts = umap_model.fit_transform(x_trained_ts)
+
+#kmeans = KMeans(n_clusters=8, algorithm='elkan')
+kmeans = kme(n_clusters=8,init='kmeans++', alg='elkan')
+#kmeans = dbk(n_clusters=8,init='random', alg='elkan')
+#kmeans = kma(k=8, method='classic')
+#kmeans = dbs()
+
+'''
+start_time = time.time()
+y_pred = kmeans.fit_predict(x_trained_ts)
+end_time = time.time()
+elapsed_time = end_time - start_time
 
 
-'''pca = PCA(n_components=2) 
-x_trained_pca = pca.fit_transform(x_trained) 
-x_tested_pca = pca.transform(x_tested)'''
-#previously used PCA dim reduction, might use again so keeping it around
+print(f"KMeans clustering took {elapsed_time:.4f} seconds")
 
-tsne = TSNE(n_components=2, random_state=11)
-x_trained_ts = tsne.fit_transform(x_trained)
+print("Adjusted Rand Index (ARI):", adjusted_rand_score(y_train, y_pred))
 
-clf = DBSCAN(eps=3, min_samples=8) #these parameters need to be tweaked
-y_pred = clf.fit_predict(x_trained_ts)
-accuracy_score(y_pred, y_train)
-print(classification_report(y_pred,y_train))
+score = silhouette_score(x_trained_ts, y_pred)
+print("Silhouette Score:", score)'''
 
-scatter = plt.scatter(x_trained_ts[:, 0], x_trained_ts[:, 1], c=y_pred, cmap='viridis', marker='.')
-plt.colorbar(scatter)
-plt.title('DBSCAN Clustering Results')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.show()
+results = []
+def k_test():
+    i=0
+    while i <1:
+        i+=1
+        kmeans =  kme(n_clusters=2,init='random' )
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time = time.time() - start_time
+
+        ari = adjusted_rand_score(y_train, y_pred)
+        score = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=4,init='random')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time2 = time.time() - start_time
+
+        ari2 = adjusted_rand_score(y_train, y_pred)
+        score2 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=8,init='random')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time3 = time.time() - start_time
+
+        ari3 = adjusted_rand_score(y_train, y_pred)
+        score3 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=16,init='random')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time4 = time.time() - start_time
+        ari4 = adjusted_rand_score(y_train, y_pred)
+        score4 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=32,init='random')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time5 = time.time() - start_time
+        ari5 = adjusted_rand_score(y_train, y_pred)
+        score5 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=64,init='random')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time6 = time.time() - start_time
+        ari6 = adjusted_rand_score(y_train, y_pred)
+        score6 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=128,init='random')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time7 = time.time() - start_time
+        ari7 = adjusted_rand_score(y_train, y_pred)
+        score7 = silhouette_score(x_trained_ts, y_pred)
+
+        results.append({
+            "k": 2,
+            "runtime_sec": elapsed_time,
+            "adjusted_rand_index": ari,
+            "silhouette_score":score,
+            "n":"",
+
+            "runtime_sec2": elapsed_time2,
+            "adjusted_rand_index2": ari2,
+            "silhouette_score2": score2,
+            "n2": "",
+
+            "runtime_sec3": elapsed_time3,
+            "adjusted_rand_index3": ari3,
+            "silhouette_score3": score3,
+            "n3": "",
+            "runtime_sec4": elapsed_time4,
+            "adjusted_rand_index4": ari4,
+            "silhouette_score4": score4,
+            "n4": "",
+            "runtime_sec5": elapsed_time5,
+            "adjusted_rand_index5": ari5,
+            "silhouette_score5": score5,
+            "n5": "",
+            "runtime_sec6": elapsed_time6,
+            "adjusted_rand_index6": ari6,
+            "silhouette_score6": score6,
+            "n6": "",
+            "runtime_sec7": elapsed_time7,
+            "adjusted_rand_index7": ari7,
+            "silhouette_score7": score7,
+        })
+
+    results.append({
+            "k": "",
+            "runtime_sec": "",
+            "adjusted_rand_index": "",
+            "silhouette_score": ""
+        })
+def dbk_test():
+    kmeans = dbk(n_clusters=8, init='random', alg='elkan')
+    start_time = time.time()
+    y_pred = kmeans.fit_predict(x_trained_ts)
+    elapsed_time = time.time() - start_time
+    ari = adjusted_rand_score(y_train, y_pred)
+    score = silhouette_score(x_trained_ts, y_pred)
+
+    results.append({
+            "k": 2,
+            "runtime_sec": elapsed_time,
+            "adjusted_rand_index": ari,
+            "silhouette_score":score,
+            "n":"",
+        })
+
+dbk_test()
+kmeans = dbk(n_clusters=8,init='random', alg='elkan')
+# === Save to CSV ===
+results_df = pd.DataFrame(results)
+results_df.to_csv("kmeans_test_results_k2.csv", index=False)
+print("Results saved to kmeans_test_results.csv")
+
