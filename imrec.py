1280d81bb69c7352db32e8f562ef174b87369712
RevolvingOcelot <122363827+RevolvingOcelot@users.noreply.github.com>
2025-07-07 20:37:16 +0300

    Add files via upload

--- imrec.py
+++ imrec.py
@@ -21,83 +21,55 @@ from sklearn.cluster import KMeans
 import umap
 import numpy as np
 from sklearn.metrics import silhouette_score
+import sys
 
 from kmeans import KMeans as kme
 
 import os
 import time
 from DBKMEANS import KMeans as dbk
+results = []
+results2 = []
+def dataprep(dim=2):
+
+    data = pd.read_pickle('./datae1.pkl')
+    x_train, x_test, y_train, y_test = train_test_split(data["embed"], data["label"], test_size=0.2, shuffle=True)
+
+    x_train = np.array(x_train)
+    x_test = np.array(x_test)
+
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    x_train = x_train[:8000]
+    y_train = y_train[:8000] #these splits arent actually necessary with DBSCAN but will keep around for now as well
+    x_test = x_test[:2000]
+    y_test = y_test[:2000]
 
 
-data = pd.read_pickle('./datae1.pkl')
-x_train, x_test, y_train, y_test = train_test_split(data["embed"], data["label"], test_size=0.2, shuffle=True, random_state=11)
+        # File to store preprocessed data
+    reduced_file = "pca_tsne_outputs.npz"
 
-x_train = np.array(x_train)
-x_test = np.array(x_test)
-
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-
-x_train = x_train[:8000]
-y_train = y_train[:8000] #these splits arent actually necessary with DBSCAN but will keep around for now as well
-x_test = x_test[:2000]
-y_test = y_test[:2000]
+    to_save = False
 
 
-# File to store preprocessed data
-reduced_file = "pca_tsne_outputs.npz"
-
-to_save = False
-
-if os.path.exists(reduced_file) and to_save is True:
-    print("Loading cached PCA and t-SNE outputs")
-    data_reduced = np.load(reduced_file)
-    x_trained_ts = data_reduced["x_tsne"]
-    # Optional: x_pca = data_reduced["x_pca"] if you need it later
-else:
-
-    # Step 1: PCA
+            # Step 1: PCA
     pca = PCA(n_components=50)
     x_pca = pca.fit_transform(x_train)
 
-    # Step 2: t-SNE
-    tsne = TSNE(n_components=2, random_state=42)
-    x_trained_ts = tsne.fit_transform(x_pca)
-
-    # Save results
-    np.savez(reduced_file, x_pca=x_pca, x_tsne=x_trained_ts)
-    print(f"Saved PCA and t-SNE outputs to {reduced_file}")
-
-#umap_model = umap.UMAP(n_components=2, random_state=42)
-#x_trained_ts = umap_model.fit_transform(x_trained_ts)
-
-#kmeans = KMeans(n_clusters=8, algorithm='elkan')
-kmeans = kme(n_clusters=8,init='kmeans++', alg='elkan')
-#kmeans = dbk(n_clusters=8,init='random', alg='elkan')
-#kmeans = kma(k=8, method='classic')
-#kmeans = dbs()
-
-'''
-start_time = time.time()
-y_pred = kmeans.fit_predict(x_trained_ts)
-end_time = time.time()
-elapsed_time = end_time - start_time
+            # Step 2: t-SNE
+    tsne = TSNE(n_components=dim)
+    return tsne.fit_transform(x_pca),y_train
 
 
-print(f"KMeans clustering took {elapsed_time:.4f} seconds")
 
-print("Adjusted Rand Index (ARI):", adjusted_rand_score(y_train, y_pred))
 
-score = silhouette_score(x_trained_ts, y_pred)
-print("Silhouette Score:", score)'''
-
-results = []
-def k_test():
+def k_test(iter=50,dim = 2):
     i=0
-    while i <1:
+    while i <iter:
         i+=1
         kmeans =  kme(n_clusters=2,init='random' )
-
+        x_trained_ts,y_train = dataprep(dim)
         start_time = time.time()
         y_pred = kmeans.fit_predict(x_trained_ts)
         elapsed_time = time.time() - start_time
@@ -187,33 +159,113 @@ def k_test():
             "adjusted_rand_index7": ari7,
             "silhouette_score7": score7,
         })
+    results_df2 = pd.DataFrame(results2)
+    results_df2.to_csv("kmeans_k" + str(dim) + ".csv", index=False)
 
-    results.append({
-            "k": "",
-            "runtime_sec": "",
-            "adjusted_rand_index": "",
-            "silhouette_score": ""
+
+def k_alg_test(iter=50,dim=2):
+    i=0
+    while i<iter:
+        i+=1
+        kmeans = kme(n_clusters=8, alg='elkan')
+        x_trained_ts, y_train = dataprep(dim)
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time1 = time.time() - start_time
+
+        ari1 = adjusted_rand_score(y_train, y_pred)
+        score1 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=8, alg='lloyd')
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time2 = time.time() - start_time
+
+        ari2 = adjusted_rand_score(y_train, y_pred)
+        score2 = silhouette_score(x_trained_ts, y_pred)
+
+        results2.append({
+            "a1": "elkan",
+            "runtime_sec": elapsed_time1,
+            "adjusted_rand_index": ari1,
+            "silhouette_score": score1,
+
+            "a2": "lloyd",
+            "runtime_sec2": elapsed_time2,
+            "adjusted_rand_index2": ari2,
+            "silhouette_score2": score2,
         })
-def dbk_test():
-    kmeans = dbk(n_clusters=8, init='random', alg='elkan')
-    start_time = time.time()
-    y_pred = kmeans.fit_predict(x_trained_ts)
-    elapsed_time = time.time() - start_time
-    ari = adjusted_rand_score(y_train, y_pred)
-    score = silhouette_score(x_trained_ts, y_pred)
+        results_df2 = pd.DataFrame(results2)
+        results_df2.to_csv("kmeans_alg"+str(dim)+".csv", index=False)
 
-    results.append({
-            "k": 2,
-            "runtime_sec": elapsed_time,
-            "adjusted_rand_index": ari,
-            "silhouette_score":score,
-            "n":"",
+def k_init_test(iter=50,dim=2,maxit=500):
+    i=0
+    while i<iter:
+        i+=1
+        kmeans = kme(n_clusters=8, alg='lloyd', init='random',max_iter=maxit)
+        x_trained_ts, y_train = dataprep(dim)
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time1 = time.time() - start_time
+
+        ari1 = adjusted_rand_score(y_train, y_pred)
+        score1 = silhouette_score(x_trained_ts, y_pred)
+
+        kmeans = kme(n_clusters=8, alg='lloyd', init='kmeans++',max_iter=maxit)
+
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time2 = time.time() - start_time
+
+        ari2 = adjusted_rand_score(y_train, y_pred)
+        score2 = silhouette_score(x_trained_ts, y_pred)
+
+        results2.append({
+            "a1": "elkan",
+            "runtime_sec": elapsed_time1,
+            "adjusted_rand_index": ari1,
+            "silhouette_score": score1,
+
+            "a2": "lloyd",
+            "runtime_sec2": elapsed_time2,
+            "adjusted_rand_index2": ari2,
+            "silhouette_score2": score2,
         })
+        results_df2 = pd.DataFrame(results2)
+        results_df2.to_csv("kmeans_init"+str(dim)+"_maxit"+str(maxit)+".csv", index=False)
 
-dbk_test()
-kmeans = dbk(n_clusters=8,init='random', alg='elkan')
-# === Save to CSV ===
-results_df = pd.DataFrame(results)
-results_df.to_csv("kmeans_test_results_k2.csv", index=False)
-print("Results saved to kmeans_test_results.csv")
+def dbk_test(iter=50,dim=2):
+    i=0
+    while i<iter:
+        i+=1
+        x_trained_ts, y_train = dataprep(dim=2)
+        kmeans = dbk(n_clusters=8, init='random', alg='elkan')
+        start_time = time.time()
+        y_pred = kmeans.fit_predict(x_trained_ts)
+        elapsed_time = time.time() - start_time
+        a=np.array(y_pred)
+        np.set_printoptions(threshold=sys.maxsize)
+        c=np.array(y_pred)
+        print(a)
+        print(y_train)
+        ari = adjusted_rand_score(y_train, y_pred)
+        score = silhouette_score(x_trained_ts, y_pred)
+
+        results.append({
+                "k": 2,
+                "runtime_sec": elapsed_time,
+                "adjusted_rand_index": ari,
+                "silhouette_score":score,
+                "n":"",
+            })
+    results_df2 = pd.DataFrame(results2)
+    results_df2.to_csv("dbkmeans_alg" + str(dim) + ".csv", index=False)
+
+##k_test()
+k_test(iter=50)
+k_alg_test(iter=50)
+k_alg_test(iter=50,dim=3)
+k_init_test(iter=50)
+k_init_test(iter=50,maxit=1)
 
