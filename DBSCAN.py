14a5b813d3c50e69b41e1f1be17efb3a7e2188aa
samuelbown <122439091+samuelbown@users.noreply.github.com>
2025-07-13 17:09:40 +0200

    DBSCAN k-d tree final version

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,87 +1,104 @@
-import random
 import math
-import numpy as np
+
 
 class DBSCAN:
-    def fit(data_points, eps, minpts):
-        noise = [] 
-        unmarked = list(data_points)
-        marked = []
-        clusters = []
+    def fit(self, data_points, eps, minpts):
+        k = len(data_points[0])
+        root = self.setup_tree(data_points, k)
         labels = [-1] * len(data_points)
-        while len(unmarked) > 0:
-            point = random.choice(unmarked) #pick a random point
-            point_index = get_index(data_points, point) #find point index
-            marked.append(point) # mark point
-            unmarked = remove_from_array(unmarked, point)
-            d = distance(point, unmarked) #check distances between it and all the other points
-            neighbours = []
-            for p in d:
-                if p[1] < eps:
-                    neighbours.append(p[0]) #add all points below the epsilon distance to neighbours
-            if len(neighbours) < minpts:
-                noise.append(point) #if neighbours are too few set point to noise
-                labels[point_index] = -1
-                for x in neighbours: #all neighbours become noise too
-                    temp_index = get_index(data_points, x) 
-                    labels[temp_index] = -1
+        clusters = []
+        noise = []
+        visited = set()
+
+        for index, point in enumerate(data_points):
+            if index in visited:
+                continue
+            visited.add(index)
+
+            neighbours = self.nearest_neighbours(root, point, eps, k)
+                
+            if len(neighbours) + 1 < minpts:
+                noise.append(point)
             else:
-                cluster = [point]
-                cluster_index = len(clusters) #gets most recent cluster
-                labels[point_index] = cluster_index
-                for neighbour in neighbours: #for each neighbour, their neighbours are checked and added to the current set of neighbours if they fit the requirements
-                    if not check_in_array(neighbour, marked):
-                        new_d = distance(neighbour, data_points) 
-                        marked.append(neighbour)
-                        unmarked = remove_from_array(unmarked, neighbour)
-                        new_neighbours = []
-                        for p in new_d:
-                            if p[1] < eps:
-                                new_neighbours.append(p[0])
-                        if len(new_neighbours) > minpts:
-                            for new_point in new_neighbours:
-                                already_in_neighbours = False
-                                for x in neighbours:
-                                    if np.array_equal(new_point, x):
-                                        already_in_neighbours =True
-                                        break
-                                if not already_in_neighbours:
-                                    neighbours.append(new_point)
-                    isIn = False
-                    for c in clusters: #check if the neighbour exists in any clusters
-                        if check_in_array(neighbour, c):
-                            isIn = True
-                    if not isIn: #if not then append to this cluster
-                        cluster.append(neighbour)
-                    neighbour_index = get_index(data_points, neighbour)
-                    if labels[neighbour_index] == -1:
-                        labels[neighbour_index] = cluster_index
-                clusters.append(cluster)  #append cluster to list of clusters
+                cluster = []
+                self.expand_neighbours(index, data_points, point, eps, minpts, cluster, clusters, neighbours, labels, visited, root, k)
+                clusters.append(cluster)
+
         return clusters, noise, labels
+
+
+    def expand_neighbours(self, index, data_points, point, eps, minpts, cluster, clusters, neighbours, labels, visited, root, k):
+        cluster.append(data_points[index])
+        labels[index] = len(clusters)
+        queue = neighbours.copy()
+        while queue:
+            neighbour_index = queue.pop(0) #get the first neighbour index
+            if neighbour_index not in visited:
+                visited.add(neighbour_index)
+                new_neighbours = self.nearest_neighbours(root, data_points[neighbour_index], eps, k) # check all its neighbours
+                if len(new_neighbours) + 1 >= minpts: #if new neighbours are more than minpts
+                    temp_list = []
+                    for x in new_neighbours:
+                        if x not in queue:
+                            temp_list.append(x)
+                    queue.extend(temp_list) #add new neighbours to queue
+            
+            if labels[neighbour_index] == -1: #if not labeled
+                labels[neighbour_index] = len(clusters) #add to most recent cluster
+                cluster.append(data_points[neighbour_index])
+
+    def setup_tree(self, data_points, k):
+        root = None
+        for index,x in enumerate(data_points): 
+            root = insert(root, x, index, k)
+        return root
+
+    def nearest_neighbours(self, root, point, eps, k, depth=0, neighbours = None):
+        if root is None:
+            return neighbours
         
-def remove_from_array(pointarray, point): #these functions are necessary because these operations don't work on np arrays easily
-    newarray = []
-    for x in pointarray:
-        if not np.array_equal(x, point):
-            newarray.append(x)
-    return newarray
+        if neighbours is None:
+            neighbours = []
 
-def check_in_array(pointarray, point):
-    for x in pointarray:
-        if np.array_equal(point, x):
-            return True
-    return False
+        d = self.distance(point, root.point)
+        if d <= eps:
+            neighbours.append(root.index) 
+        cd = depth % k #cd - current dimension, each layer of the tree the dimension changes to narrow down on the point quicker
+        
+        if point[cd] < root.point[cd]: #if x/y of the point is less/more than the x/y of the current node then go left/right down the tree 
+            self.nearest_neighbours(root.left, point, eps, k, depth + 1, neighbours) 
+            if abs(point[cd] - root.point[cd]) <= eps: #if the current node is in eps distance of the point, there may be another point within eps on the other side of the tree, obscured by the dimension cutoff
+                self.nearest_neighbours(root.right, point, eps, k, depth + 1, neighbours)
+        else:
+            self.nearest_neighbours(root.right, point, eps, k, depth + 1, neighbours)
+            if abs(point[cd] - root.point[cd]) <= eps:
+                self.nearest_neighbours(root.left, point, eps, k, depth + 1, neighbours)
+        return neighbours
 
-def get_index(pointarray, point):
-    for index, x in enumerate(pointarray):
-        if np.array_equal(x, point):
-            return index
-    return -1
+    def distance(self, point1 = [], point2 = []):
+        total = 0
+        for n in range(0, len(point1)):
+            total += (point1[n] - point2[n])**2
+        return math.sqrt(total)
 
-def distance(point1, data):
-    distances = []
-    for point in data:
-        d = math.sqrt((point[0] - point1[0])**2 + (point[1] - point1[1])**2) #sqrt((x2-x1)^2 + (y2-y1)^2)
-        if d != 0:
-            distances.append([point, d])
-    return np.array(distances, dtype=object)
+class Node:
+    def __init__(self, point, index):
+        self.point = point
+        self.left = None
+        self.right = None
+        self.index = index
+
+def newNode(point, index):
+    return Node(point, index)
+
+def insert(root, point, index, k, depth = 0): 
+    if not root:
+        return newNode(point, index)
+
+    cd = depth % k
+    
+    if point[cd] < root.point[cd]: 
+        root.left = insert(root.left, point, index, k, depth + 1)
+    else:
+        root.right = insert(root.right, point, index, k, depth + 1)
+    return root
